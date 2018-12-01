fc1de474919949483b3c5d51579bdf0bef5d0fec
YuriyB <paganism@mail.ru>
2018-12-01 14:33:30 +0300

    added catalog to navbar

--- bookstore/store/views.py
+++ bookstore/store/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Book, Author
+from .models import Book, Author, GenreGroups
 
 
 # Create your views here.
 class BookListView(generic.ListView):
-    model = Book
+    model = Book, GenreGroups
     paginate_by = 3
+    template_name = 'store/book_list.html'
 
     def get_queryset(self):
-        return Book.objects.all()
+        book_list = Book.objects.all()
+        return book_list
     
     def get_context_data(self, **kwargs):
         context = super(BookListView, self).get_context_data(**kwargs)
-        context['some_data'] = 'This is some data'
+        context['genregroups_list'] = GenreGroups.objects.filter(id__gt=1)
+        print(context)
         return context
+
 
 class BookDetailView(generic.DetailView):
     model = Book
